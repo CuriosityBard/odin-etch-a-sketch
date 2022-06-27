f18b6c3987c008be2ca02a320273529df2d2c386
CuriosityBard <curiositybard@gmail.com>
2022-06-27 09:00:59 -0700

    Add resolution changing functionality

--- scripts.js
+++ scripts.js
@@ -5,18 +5,32 @@ function makeGrid(w) {
 
     // sizing the pixels
     const boxSize = 640 / w;
-    container.style.width = boxSize;
-    container.style.height = boxSize; 
 
     // styling the grid
-    container.style.gridTemplateRows = "repeat(1fr, " + w + ")";
-    container.style.gridTemplateColumns = "repeat(1fr, " + w + ")";
+    const gridTemplate = "1fr ".repeat(w);
+    container.style.gridTemplateRows = gridTemplate;
+    container.style.gridTemplateColumns = gridTemplate;
+
+    // clear previous grid
+    container.innerHTML = "";
 
     for (let i = 0; i < (w*w); i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('pixel');  // for styling
+        newDiv.style.height = boxSize + 'px';
+        newDiv.style.width = boxSize + 'px';
 
         container.appendChild(newDiv);
     }
 }
 makeGrid(16);
+
+function changeResolution() {
+    let newRes = prompt("How many squares per side?");
+
+    while (parseInt(newRes) > 100) {
+        newRes = prompt("Please enter a number under 100.\nHow many squares per side?");
+    }
+    
+    makeGrid(newRes);
+}
