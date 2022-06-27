48644589f961fad85be77b7ce99abd66adeae5a4
CuriosityBard <curiositybard@gmail.com>
2022-06-27 08:50:01 -0700

    makeGrid now uses custom width/height

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 // create grid of divs 
-function makeGrid() {
+function makeGrid(w) {
+    // w = number of squares per side
     const container = document.querySelector('#canvas');
 
-    for (let i = 0; i < (16*16); i++) {
+    // sizing the pixels
+    const boxSize = 640 / w;
+    container.style.width = boxSize;
+    container.style.height = boxSize; 
+
+    // styling the grid
+    container.style.gridTemplateRows = "repeat(1fr, " + w + ")";
+    container.style.gridTemplateColumns = "repeat(1fr, " + w + ")";
+
+    for (let i = 0; i < (w*w); i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('pixel');  // for styling
 
         container.appendChild(newDiv);
     }
 }
-makeGrid();
+makeGrid(16);
